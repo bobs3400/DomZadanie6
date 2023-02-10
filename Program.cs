4bc4fbfb09acb1b8c1499515bb39e81d033b0da5
Роберт <bobs3400@mail.ru>
2023-02-11 07:31:54 +1000

    проверка четности1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите целое число");
+int A = int.Parse(Console.ReadLine());
+if (A / 2 = 0)
+{
+    Console.WriteLine("Число четное")
+}
+Console.WriteLine("Число нечетное")
